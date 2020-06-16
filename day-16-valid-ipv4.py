4b48ff01594d907c470c0bc561a879297b310dd9
jeffysam6 <jeffysam02@gmail.com>
2020-06-17 00:19:32 +0530

    upsolved by checking each individually using try and catch

--- day-16-valid-ipv4.py
+++ day-16-valid-ipv4.py
@@ -1,22 +1,35 @@
 class Solution:
     def validIPAddress(self, IP: str) -> str:
         
-        if('.' in IP):
+        
+        def check_4(n):
             
-            s = IP.split('.')
+            try:
+                return 0<=int(n)<=255 and str(int(n)) == n
             
+            except:
+                return False
             
-            for i in s:
-                if(0>i>255):
-                    return 'Neither'
+        def check_6(n):
             
+            if(len(n) > 4):
+                return False
+            
+            try:
+                return int(n,16) >= 0  and n[0] != '-'
+            
+            except:
+                return False
+                
+            
+        
+        
+        if(IP.count('.') == 3 and all(check_4(n) for n in IP.split('.'))):
             return 'IPv4'
         
-        elif(':' in IP):
-            
-            s = IP.split(':')
-            
-            for i in s:
-                if()
-            
+        elif(IP.count(':') == 7 and all(check_6(n) for n in IP.split(':'))):
+            return 'IPv6'
+        
+        else:
+            return 'Neither'
             
